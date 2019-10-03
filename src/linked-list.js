3ea5d3c2c0eb77d34b81e02a3268e99d83c0796c
Viktar Svintsitski <jetalai3@gmail.com>
2019-10-04 01:10:10 +0300

    feat: add double-linked list implementation

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,78 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.nodeArray = [];
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
+    }
 
-    append(data) {}
+    append(data) {
+        const newNode = new Node();
+        newNode.data = data;
+        if (this.isEmpty()) {
+            this._head = newNode;
+            this._tail = newNode;
+            this._tail.prev = newNode;
+            this.head.next = newNode;
+        }
+        newNode.prev = this._tail;
+        this._tail = newNode;
 
-    head() {}
+        this.nodeArray.push(newNode);
+        this.length = this.nodeArray.length;
+        return this;
+    }
 
-    tail() {}
+    head() {
+        return this._head && this._head.data;
+    }
 
-    at(index) {}
+    tail() {
+        return this._tail && this._tail.data;
+    }
 
-    insertAt(index, data) {}
+    at(index) {
+        return this.nodeArray[index].data;
+    }
 
-    isEmpty() {}
+    insertAt(index, data) {
+        const newNode = new Node();
+        newNode.data = data;
+        this.nodeArray.splice(index, 0, newNode);
+        this.length += 1;
+        return this;
+    }
 
-    clear() {}
+    isEmpty() {
+        return this.length === 0 ? true : false;
+    }
 
-    deleteAt(index) {}
+    clear() {
+        this.nodeArray = [];
+        this._head = null;
+        this._tail = null;
+        this.length = 0;
+        return this;
+    }
 
-    reverse() {}
+    deleteAt(index) {
+        this.nodeArray.splice(index - 1, 1);
+        return this;
+    }
 
-    indexOf(data) {}
+    reverse() {
+        let nd = this._head;
+        this._head = this._tail;
+        this._tail = nd;
+        this.nodeArray.reverse();
+        return this;
+    }
+
+    indexOf(data) {
+        return this.nodeArray.indexOf(this.nodeArray.filter(el => el.data === data)[0]);
+    }
 }
 
 module.exports = LinkedList;
